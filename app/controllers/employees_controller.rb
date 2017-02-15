dcef18af80133dce4449457c7a7f487a78c250ac
Carlos <school@Carlos-MacBook-Air.local>
2017-02-14 21:19:22 -0800

    Added authentication and secure token to header

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,30 +1,38 @@
 class EmployeesController < ApplicationController
   def index
-    @employees = Unirest.get("http://localhost:3000/api/v2/employees").body
+    @employees = Employee.all
+    
   end
+
   def show
     id = params[:id]
-    @employee = Unirest.get("http://localhost:3000/api/v2/employees/#{id}").body
+    @employee = Employee.find(id)
   end
+
   def new
     render :new
   end
+
   def create
     @employee = Unirest.post("http://localhost:3000/api/v2/employees", :parameters => {first_name: params[:first_name], last_name: params[:last_name], email: params[:email]}).body
     redirect_to employee_path(@employee['id'])
   end
+
   def edit
     id = params[:id]
     @employee = Unirest.get("http://localhost:3000/api/v2/employees/#{id}").body
   end
+
   def update
     id = params[:id]
     @employee = Unirest.patch("http://localhost:3000/api/v2/employees/#{id}", :parameters => {first_name: params[:first_name], last_name: params[:last_name], email: params[:email]}).body
     redirect_to employee_path(@employee['id'])
   end
+
   def destroy
     id = params[:id]
-    @employee = Unirest.delete("http://localhost:3000/api/v2/employees/#{id}").body
+    @employee = Employee.find(id)
+    @employee.destroy
     redirect_to employees_path
   end
 
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -8,6 +8,30 @@ class Employee
     @birthday = ['birthday']
     @addresses = ['addresses']
   end
+
+  
+  def self.find(id)
+    employee_hash = Unirest.get("#{ENV['API_BASE_URL']}/employees/#{id}", :headers => {"X-User-Email" => ENV['API_EMAIL'], "Authorization" => ENV['API_KEY'}).body
+    Employee.new(employee_hash)
+  end
+
+  def self.all
+    employees = []
+    api_employees = Unirest.get("http://localhost:3000/api/v2/employees", :headers => {"X-User-Email" => "test@test", "Authorization" => "Token token=abc"}).body
+    api_employees.each do |employee_hash|
+      employees << Employee.new(employee_hash)
+    end
+    employees
+  end
+
+  def destroy
+    Unirest.delete("http://localhost:3000/api/v2/employees/#{id}").body
+  end
+
+  def friendly_birthday
+    birthdate = Date.parse(@birthday)
+    birthdate.strftime('%b %d, %Y')
+  end
 end
 
 
